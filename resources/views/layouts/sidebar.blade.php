2b763f80938c6c80775752a86fbbc6bc52e4440c
Guilherme Silveira Rego <up202207041@up.pt>
2024-11-25 20:13:56 +0000

    remove app.js

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -27,7 +27,6 @@
             // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
         </script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="{{ url('js/app.js') }}" defer></script>
     </head>
 
     <body>
