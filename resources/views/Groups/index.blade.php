c75c6c31a72f17a9c3cbee2c8b79318963bde47a
up202208437 <up202208437@edu.up.pt>
2024-12-16 21:33:20 +0000

    correction in groups management button

--- resources/views/Groups/index.blade.php
+++ resources/views/Groups/index.blade.php
@@ -5,12 +5,10 @@
 
 <div class="container p-4" style="overflow-y: scroll">
     <div class="profile-header container d-flex flex-column">
-        <div class="image-container w-100 p-4 d-flex align-items-end" 
+        <div class="image-container w-100 p-4 d-flex align-items-end"
             style="height: 250px; background-image: url('{{ $group->groupBanner ? asset('storage/' . $group->groupBanner->url) : asset('images/banner-default.png') }}');">
-        <img src="{{ $group->groupImage ? asset('storage/' . $group->groupImage->url) : asset('images/group-default.png') }}"
-            class="img-thumbnail rounded-circle mb-3" 
-            alt="Group Image"
-            style="width: 150px; height: 150px;">
+            <img src="{{ $group->groupImage ? asset('storage/' . $group->groupImage->url) : asset('images/group-default.png') }}"
+                class="img-thumbnail rounded-circle mb-3" alt="Group Image" style="width: 150px; height: 150px;">
         </div>
         <div class="identification p-2">
             <h2>{{ $group->name }}</h2>
@@ -23,14 +21,15 @@
                 @else
                     Members
                 @endif
-            </div>
+        </div>
         </p>
         <div class="interactions d-flex gap-2 pb-3 border-bottom">
             @auth
                 @if ($isOwner || auth()->user()->isAdmin())
                     <a href="{{ route('group.edit', $group->id) }}" class="button btn-primary m-0">Edit Group</a>
-                    <a href="{{ route('group-management-members.index', $group->id) }}" class="button btn-primary m-0">Manage
-                        Group</a>
+                    @if ($isOwner) 
+                        <a href="{{ route('group-management-members.index', $group->id) }}" class="button btn-primary m-0">Manage Group</a>
+                    @endif
                     <form action="{{ route('group.destroy', $group->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
@@ -59,54 +58,54 @@
                         @method('DELETE')
                         <button type="submit" class="button btn-danger m-0">Cancel Join Request</button>
                     </form>
-    
+
                 @endif
             @endauth
         </div>
     </div>
-@if (!$group->is_private || ($group->is_private && ($isMember || auth()->user()?->isAdmin())))
-    <div class="post-container pt-3">
-        @if(session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        @endif
-        @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
-        @if ($isMember)
-        <div class="createpost mb-4">
-            <div class="post">
-                <div class="post-body">
-                    <form action="{{ route('post.store.group', $group->id) }}" method="POST">
-                        @csrf
-                        <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
-                        <input type="hidden" name="date_time" value="{{ now() }}">
-                        <div class="form-group mb-3">
-                            <textarea class="form-control" id="content" name="content" rows="3" required
-                                placeholder="Write something..."></textarea>
-                        </div>
-                        <input type="hidden" name="group_id" value="{{ $group->id}}">
-                        <button type="submit" class="btn btn-primary">Post</button>
-                    </form>
+    @if (!$group->is_private || ($group->is_private && ($isMember || auth()->user()?->isAdmin())))
+        <div class="post-container pt-3">
+            @if(session('error'))
+                <div class="alert alert-danger">
+                    {{ session('error') }}
                 </div>
-            </div>
-        </div>
-@endif
-    @if($posts->count() == 0)
-        <div class="alert alert-info">
-            No posts yet.
+            @endif
+            @if(session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
+            @if ($isMember)
+                <div class="createpost mb-4">
+                    <div class="post">
+                        <div class="post-body">
+                            <form action="{{ route('post.store.group', $group->id) }}" method="POST">
+                                @csrf
+                                <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+                                <input type="hidden" name="date_time" value="{{ now() }}">
+                                <div class="form-group mb-3">
+                                    <textarea class="form-control" id="content" name="content" rows="3" required
+                                        placeholder="Write something..."></textarea>
+                                </div>
+                                <input type="hidden" name="group_id" value="{{ $group->id}}">
+                                <button type="submit" class="btn btn-primary">Post</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            @endif
+            @if($posts->count() == 0)
+                <div class="alert alert-info">
+                    No posts yet.
+                </div>
+            @else
+                @foreach($posts as $post)
+                    @include('post.post')
+                @endforeach
+            @endif
         </div>
     @else
-        @foreach($posts as $post)
-            @include('post.post')
-        @endforeach
+        <h2>This is a private group</h2>
     @endif
-    </div>
-@else
-    <h2>This is a private group</h2>
-@endif
 </div>
 @endsection
