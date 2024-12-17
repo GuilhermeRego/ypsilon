74c134a7b05910d51ff26108998d949867275dee
Yussee-1 <up202207784@up.pt>
2024-12-17 22:04:58 +0000

    Started Implementation of Private Posts

---
+++ app/Policies/PrivacyPolicy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Post;
+
+class PrivacyPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function view(User $user, Post $post)
+    {
+        return true;
+    // // If the user is the author, allow viewing
+    // if ($user->id === $post->user_id) {
+    //     return true;
+    // }
+
+    // // If the post's user account is not private, allow viewing
+    // if (!$post->user->is_private) {
+    //     return true;
+    // }
+
+    // // If the account is private, check if the current user follows the author
+    // if ($post->user->is_private && !$user->following->contains('followed_id', $post->user_id)) {
+    //     return false;
+    // }
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 use App\Models\Group;
 use App\Policies\GroupPolicy;
+use App\Policies\PrivacyPolicy;
 use Illuminate\Support\Facades\Gate;
-// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -16,6 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Group::class => GroupPolicy::class,
+        Post::class => PrivacyPolicy::class,
     ];
 
     /**
