7066cb8bf8db47a73741c31460f52b90432c42d8
coltginger <gnbarroso@gmail.com>
2024-12-17 21:40:27 +0000

    Like and Dislike Notifications are recieved

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -1,3 +1,5 @@
+<!-- resources/views/notifications/index.blade.php -->
+
 @extends('layouts.app')
 
 @section('content')
@@ -6,7 +8,18 @@
     @if($notifications->isEmpty())
         <p>No notifications found.</p>
     @else
-        <p>No notifications found.</p>
+        <ul class="list-group">
+            @foreach($notifications as $notification)
+                <li class="list-group-item">
+                    <a href="{{ route('post.show', ['post' => $notification->reaction->post_id]) }}">
+                        {{ $notification->reaction->user->nickname }} 
+                        {{ $notification->reaction->is_like ? 'liked' : 'disliked' }} 
+                        your post.
+                    </a>
+                    <small class="text-muted">{{ $notification->date_time }}</small>
+                </li>
+            @endforeach
+        </ul>
     @endif
 </div>
 @endsection
