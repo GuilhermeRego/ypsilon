00b90e13d95783dcf5a9832d04ffbf046ebbaab2
Guilherme Silveira Rego <up202207041@up.pt>
2024-12-21 12:37:46 +0000

    Finished reports and admin page

--- resources/views/Groups/index.blade.php
+++ resources/views/Groups/index.blade.php
@@ -76,23 +76,7 @@
                 </div>
             @endif
             @if ($isMember)
-                <div class="createpost mb-4">
-                    <div class="post">
-                        <div class="post-body">
-                            <form action="{{ route('post.store.group', $group->id) }}" method="POST">
-                                @csrf
-                                <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
-                                <input type="hidden" name="date_time" value="{{ now() }}">
-                                <div class="form-group mb-3">
-                                    <textarea class="form-control" id="content" name="content" rows="3" required
-                                        placeholder="Write something..."></textarea>
-                                </div>
-                                <input type="hidden" name="group_id" value="{{ $group->id}}">
-                                <button type="submit" class="btn btn-primary">Post</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                @include('post.create')
             @endif
             @if($posts->count() == 0)
                 <div class="alert alert-info">
