3788330da52a48ccd2f202dbca7ddb219b5d9fa8
Yussee-1 <up202207784@up.pt>
2024-12-22 19:05:57 +0000

    Comments fix

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -8,7 +8,7 @@
 
 
     <!-- Create Comment -->
-    <div class="comments" style="overflow-y: scroll">
+    <div class="comments p-4" style="overflow-y: scroll">
         @auth
             @if ($post->group)
                 @if (auth()->user()->can('isMember', $post->group))
@@ -31,42 +31,50 @@
         <!-- Comments -->
         <div class="comments mt-3">
             @forelse ($post->comments as $comment)
-                <div class="comment mt-3 p-3 bg-white rounded shadow-sm">
-                    <div class="comment-body">
-                        <div class="comment-author d-flex flex-row align-items-center mb-2">
-                            @if ($comment->user_id == null)
-                                <h5 class="comment-username">Anonymous</h5>
-                            @else
-                                <a href="{{ route('profile.show', ['username' => $comment->user->username]) }}" style="font-size: 1.5em; text-decoration:none">
-                                    {{ $comment->user->nickname }}</a>
-                                <p class="m-0 pl-1">&#64{{ $comment->user->username }}</p>
-                            @endif
-                        </div>
-                        <p class="comment-content">{!! $comment->content !!}</p>
-                        <p class="comment-date"><small class="text-muted">{{ $comment->date_time }}</small></p>
-                        @auth
-                            @if (auth()->check() && (auth()->user()->id == $comment->user_id || auth()->user()->isAdmin()))
-                                <div class="comment-actions mt-3">
-                                    <a href="{{ route('comment.edit', ['comment' => $comment->id]) }}"
-                                        class="btn btn-outline-primary btn-sm mb-0"><i class="bi bi-pencil-square"></i> Edit</a>
-                                    <form action="{{ route('comment.destroy', ['comment' => $comment->id]) }}" method="POST"
-                                        class="d-inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-outline-danger btn-sm"><i class="bi bi-trash"></i>
-                                            Delete</button>
-                                    </form>
-                                </div>
-                            @endif
-                            @if (auth()->user()->id != $comment->user_id)
-                                <div class="comment-actions mt-3">
-                                    <a href="{{ route('report.comment', ['comment' => $comment->id]) }}"
-                                        class="btn btn-outline-danger btn-sm"><i class="bi bi-flag"></i> Report</a>
-                                </div>
-                            @endif
-                        @endauth
+            <div class="col-md-10 d-flex flex-column justify-content-between pb-3 mb-3 border border-1 rounded">
+                <div class="post-body">
+                    <!-- If the user id is null, it is anonymous, otherwise, display the username -->
+                    <div class="post-author d-flex flex-row align-items-center mb-3">
+                        @if ($comment->user_id == null)
+                            <h5 class="post-username">Anonymous</h5>
+                        @else
+                            <img src="{{ $comment->user->profileImage ? asset($comment->user->profileImage->url) : asset('images/profile-default.png') }}"
+                            class="border border-2 rounded-circle mr-2" alt="Profile Image"
+                            style="width: 40px; height: 40px;">
+                            <a href="{{ route('profile.show', ['username' => $comment->user->username]) }}" style="font-size: 1.5em; text-decoration: none" >
+                                {{ $comment->user->nickname }}</a>
+                            <p class="m-0 pl-1 fw-light" style="color: #a4a4a4">&#64{{ $comment->user->username}}</p>
+                        @endif
                     </div>
+                    <div>{!! $comment->content !!}</div>
+                    <p class="post-date"><small class="text-muted">{{ $comment->date_time }}</small></p>
+                    </a>
                 </div>
+                <div class="post-footer d-flex gap-2">
+                @auth
+                    @if (auth()->check() && auth()->user()->id == $comment->user_id || auth()->user()->isAdmin())
+                        <div class="post-edit">
+                            <a href="{{ route('comment.edit', ['comment' => $comment->id]) }}" class="btn btn-outline-primary btn-sm mb-0"><i
+                                    class="bi bi-pencil-square"></i> Edit</a>
+                        </div>
+                        <div class="post-delete">
+                            <form action="{{ route('comment.destroy', ['comment' => $comment->id]) }}" method="POST" class="mb-0">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-outline-danger btn-sm" style="height: fit-content"><i class="bi bi-trash"></i> Delete</button>
+                            </form>
+                        </div>
+                    @endif
+                    @if (auth()->user()->id != $comment->user_id)
+                        <div class="post-report">
+                            <a href="{{ route('comment.post', ['comment' => $comment->id]) }}" class="btn btn-outline-danger btn-sm"><i
+                                    class="bi bi-flag"></i> Report
+                            </a>
+                        </div>
+                    @endif
+                @endauth
+                </div>
+            </div>
             @empty
                 <div class="alert alert-info">
                     No comments yet.
